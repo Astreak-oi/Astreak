2c8c576c571bfd98f3fa1971298e076f9834e1b0
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-03-06 23:49:12 +0530

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind"; // https://astreak.build/config
 
-import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
-// https://astreak.build/config
+// https://astro.build/config
 export default defineConfig({
-
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
